8455d9d68adb1b0623b42b9661f21b7b20b473aa
Tom Turner <tom@m3ter.com>
2025-03-07 08:25:54 +0000

    feat: BR-2702 - async token fetching (#86)

--- m3ter-java-core/src/main/kotlin/com/m3ter/sdk/core/PrepareRequest.kt
+++ m3ter-java-core/src/main/kotlin/com/m3ter/sdk/core/PrepareRequest.kt
@@ -5,6 +5,8 @@ package com.m3ter.sdk.core
 import com.m3ter.sdk.core.http.HttpRequest
 import com.m3ter.sdk.models.AuthenticationGetBearerTokenParams
 import com.m3ter.sdk.models.AuthenticationGetBearerTokenResponse
+import com.m3ter.sdk.services.async.AuthenticationServiceAsync
+import com.m3ter.sdk.services.async.AuthenticationServiceAsyncImpl
 import com.m3ter.sdk.services.blocking.AuthenticationService
 import com.m3ter.sdk.services.blocking.AuthenticationServiceImpl
 import java.util.*
@@ -24,7 +26,7 @@ internal fun HttpRequest.prepare(clientOptions: ClientOptions, params: Params): 
         request.pathSegments.contains("oauth") && request.pathSegments.contains("token")
     val token: String? = clientOptions.token
 
-    // If the request is not an OAuth token request or if the token is not null
+    // If the request is an OAuth token request or if the token is not null
     // return early, no need to generate token.
     if (isAuthRequest || token != null) {
         return request
@@ -52,7 +54,43 @@ internal fun HttpRequest.prepare(clientOptions: ClientOptions, params: Params): 
 internal fun HttpRequest.prepareAsync(
     clientOptions: ClientOptions,
     params: Params,
-): CompletableFuture<HttpRequest> =
-    // This async version exists to make it easier to add async specific preparation logic in the
-    // future.
-    CompletableFuture.completedFuture(prepare(clientOptions, params))
+): CompletableFuture<HttpRequest> {
+    val request: HttpRequest =
+        toBuilder()
+            .putAllQueryParams(clientOptions.queryParams)
+            .replaceAllQueryParams(params._queryParams())
+            .putAllHeaders(clientOptions.headers)
+            .replaceAllHeaders(params._headers())
+            .build()
+
+    val isAuthRequest: Boolean =
+        request.pathSegments.contains("oauth") && request.pathSegments.contains("token")
+    val token: String? = clientOptions.token
+
+    // If the request is an OAuth token request or if the token is not null
+    // return early, no need to generate token.
+    if (isAuthRequest || token != null) {
+        return CompletableFuture.completedFuture(request)
+    }
+
+    val keySecret: String = clientOptions.apiKey + ":" + clientOptions.apiSecret
+    val encodedKeySecret: String = Base64.getEncoder().encodeToString(keySecret.toByteArray())
+    val authService: AuthenticationServiceAsync = AuthenticationServiceAsyncImpl(clientOptions)
+
+    return authService
+        .getBearerToken(
+            AuthenticationGetBearerTokenParams.builder()
+                .grantType(AuthenticationGetBearerTokenParams.GrantType.CLIENT_CREDENTIALS)
+                .putAdditionalHeader("Authorization", "Basic $encodedKeySecret")
+                .build(),
+            RequestOptions.none(),
+        )
+        .thenApply { tokenResponse ->
+            request
+                .toBuilder()
+                .putAllHeaders(
+                    clientOptions.toBuilder().token(tokenResponse.accessToken()).build().headers
+                )
+                .build()
+        }
+}
